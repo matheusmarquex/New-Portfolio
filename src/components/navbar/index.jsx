954e6a9b17a056e0f2306138c739b40804ccadb6
Matheus Marques <mmarxdev@gmail.com>
2021-07-06 11:26:17 -0300

    Feito navbar - resposive

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,13 +4,38 @@ import logoimg from '../../assets/imgs/onigiri.png'
 import solimg from '../../assets/imgs/sol.png'
 import luaimg from '../../assets/imgs/lua.png'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
 
 
 export default function navBar() {
 
     return (
         <>
-            <div id="container-nav">
+            {/* mobile menu */}
+            <Navbar bg="myGray" variant="dark" expand="lg">
+                <Navbar.Brand className="imgNav">
+                <img src={logoimg} width="40px" height="40px"/>
+                </Navbar.Brand>
+                <Navbar.Toggle className="toggle"/>
+
+                <Navbar.Collapse>
+
+                <Nav className="itens">
+                    <Nav.Link href="">Início</Nav.Link>
+                    <Nav.Link href="">Sobre</Nav.Link>
+                    <Nav.Link href="">Skills</Nav.Link>
+                    <NavDropdown title="Projetos">
+                        <NavDropdown.Item href="Projetos/loading...">Loading...</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#">Contato</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+
+
+            {/* <div id="container-nav">
                 <div id="container-nav2">
                     <div id="logo-nav">
                         <div>
@@ -30,12 +55,9 @@ export default function navBar() {
                                 <li><a href="/">Contato</a></li>
                             </ul>
                         </nav>
-                        <div id="lua">
-                            <img src={luaimg} />
-                        </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
